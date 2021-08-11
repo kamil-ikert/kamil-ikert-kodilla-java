fe7dd18fbe0b572545093c896e9c1b143c655f2b
Kamil Ikert <kamil.ikert@gmail.com>
2021-08-11 19:34:24 +0200

    8.1 Try, Catch, Finally !

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,6 +3,7 @@ import com.kodilla.testing.calc.Calculator;
 import com.kodilla.testing.collection.OddNumberExterminator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 public class TestingMain {
     public static void main(String[] args) {
