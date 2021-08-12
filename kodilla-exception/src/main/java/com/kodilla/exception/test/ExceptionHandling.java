43d794233013773035a3972401b75630bb471c5e
Kamil Ikert <kamil.ikert@gmail.com>
2021-08-12 14:59:16 +0200

    8.3 Throw, throws

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String args[]) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            String result = secondChallenge.probablyIWillThrowException(0.5, 5.5);
+            System.out.println(result);
+        } catch(Exception e) {
+            System.out.println("Error occurred: " + e);
+        } finally {
+            System.out.println("Job is done.");
+        }
+    }
+}
+
+
