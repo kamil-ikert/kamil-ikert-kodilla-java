ced3b07a035907a1825a6b2719c8fabd1fd17488
Kamil Ikert <kamil.ikert@gmail.com>
2021-09-08 17:41:08 +0200

    10.2 Spring Calculator v2.0

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 public class CalculatorTestSuite {
 
@@ -14,10 +17,15 @@ public class CalculatorTestSuite {
     void testCalculator() {
         //Given
         //When
-        calculator.mul(2, 2);
-        calculator.add(100, 50);
-        calculator.div(100, 20);
-        calculator.sub(55, 5);
+        double result1 = calculator.mul(2, 2);
+        double result2 = calculator.add(100, 50);
+        double result3 = calculator.div(100, 20);
+        double result4 = calculator.sub(55, 5);
         //Then
+        assertEquals(4, result1);
+        assertEquals(150, result2);
+        assertEquals(5, result3);
+        assertEquals(50, result4);
+
     }
 }
